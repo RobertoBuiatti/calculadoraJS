fcda3eddc8a2610fcc285699dee0caab38b6b126
Roberto Caetano Buiatti <102183235+RobertoBuiatti@users.noreply.github.com>
2023-04-29 15:51:36 -0300

    modificação

---
+++ calculadora react/calculadora/src/App.js
@@ -0,0 +1,12 @@
+import Calculadora from './components/Calculadora';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Calculadora</h1>
+      <Calculadora />
+    </div>
+  );
+}
+
+export default App;
